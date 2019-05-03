ee3c3fa5c48a7caff9b33226a31028a459ba42f9
Ekaterina Krutakova <lagoonnw@gmail.com>
2019-05-03 11:21:59 +0500

    Правит ковычки в импорте и пр.

--- src/components/welcome-screen/welcom-screen.test.js
+++ src/components/welcome-screen/welcom-screen.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import {WelcomeScreen} from "./welcome-screen";
+import {WelcomeScreen} from './welcome-screen.jsx';
 
 describe(`welcome screen correctly renders`, () => {
   test(`renders with default props`, () => {
